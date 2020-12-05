a3aca157864e44e8faa89bf57211953f66d28301
inq39 <61730491+inq39@users.noreply.github.com>
2020-12-05 09:36:41 +0100

    MouseToWorldPosition

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _hookRb;
-    [SerializeField] private Rigidbody _playerRb;
+    [SerializeField] 
+    private Rigidbody _hookRb;
+    [SerializeField] 
+    private Rigidbody _playerRb;
     private float _maxSpring = 2.0f;
     private float _releaseTime = 0.15f;
     private bool _isTouched = false;
+    private float _objectCoordZ;
+    private Vector3 _mouseOffset;
 
     // Update is called once per frame
     void Update()
     {
         if (_isTouched)
         {
-            Vector3 _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            if (Vector3.Distance(_mousePos, _hookRb.position) > _maxSpring)
+            
+           /* if (Vector3.Distance(_mousePos, _hookRb.position) > _maxSpring)
             {
-                _playerRb.position = _hookRb.position + (_mousePos - _hookRb.position).normalized * _maxSpring;
+                //_playerRb.position = _hookRb.position + (_mousePos - _hookRb.position).normalized * _maxSpring;
             } 
             else
             {
-                _playerRb.position = _mousePos;
-            }
+               OnMouseDrag();
+            } */
         }
         
 
@@ -34,23 +37,37 @@ public class PlayerController : MonoBehaviour
     {
         _isTouched = true;
         _playerRb.isKinematic = true;
+        _objectCoordZ = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        _mouseOffset = gameObject.transform.position - GetMouseAsWorldPoint();
     }
 
     private void OnMouseUp()
     {
-        
         _isTouched = false;
         _playerRb.isKinematic = false;
         StartCoroutine(ReleaseHook());
+    }
+
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        Vector3 _mousePosition = Input.mousePosition;
+        _mousePosition.z = _objectCoordZ;
+        return Camera.main.ScreenToWorldPoint(_mousePosition);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseAsWorldPoint() + _mouseOffset;
     }
 
     IEnumerator ReleaseHook()
     {
         yield return new WaitForSeconds(_releaseTime);
       
-        //this.enabled = false;
+        this.enabled = false;
 
-        GetComponent<SpringJoint>().breakForce = 5;
+        //GetComponent<SpringJoint>().breakForce = 0;
         // next Sphere
     }
+
 }
